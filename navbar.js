55fe72822f57eb96d7e10364cc01c0eee4172b7d
Charlotte Dickson <Cddickson@student.unimelb.eduau>
2024-06-03 09:53:58 +1000

    responsive nav 1.f

--- navbar.js
+++ navbar.js
@@ -1,1 +1,10 @@
-
+<script>
+        function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+    }
+    </script>
